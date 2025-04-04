0f112d7c8de43bddf6f4a4b37d9fa3b720410f90
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2025-04-04 12:53:07 +0000

    operations in js

---
+++ Basics/04_operations.js
@@ -0,0 +1,41 @@
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2+2);
+// console.log(2%2);
+
+
+// let str = "Yashpal"
+// let str2 = "Parmar"
+// let str3 = str+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+1+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2,num3
+
+// num1=num2=num3 = 2 + 2
+
+// let gamecounter = 100
+// ++gamecounter;
+// console.log(gamecounter);
+
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
+console.log(x2,y2);
+
+
+

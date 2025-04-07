d635945e129a2eca8c3a67cd2e64676893f256b9
yashpal parmar <144250326+yashpal8078@users.noreply.github.com>
2025-04-07 05:22:59 +0000

    comparsons

---
+++ Basics/05_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2 >1);
+// console.log(2 >=1);
+// console.log(2 <=1);
+// console.log(2 ==1);
+// console.log(2 !=1);
+
+
+console.log("2" > 1);
+console.log("02" >1);
+
+// console.log(null>1);
+// console.log(null==1);
+// console.log(null>=0 );   avoid this thye of comparsion in javascript
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === strict check nott only check the value but also check the data type also 
+
+console.log("2" ===2);
